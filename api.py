a35dab2a86404f4a4f0b5da7e98d345ad2206d83
abdulrazie <abdulrazie987@gmail.com>
2026-07-17 23:06:03 +0800

    Randomized latitude and longitude for each new hikers, added status, role and heading constants

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import json
+import random
+import math
 from pathlib import Path
 
 app = FastAPI(title="Hiker Simulator API")
@@ -11,10 +13,39 @@ data_file = Path(__file__).with_name("people.json")
 def load_people():
     with open(data_file, "r") as file:
         return json.load(file)
-    
+
+
 def save_people(people):
     with open(data_file, "w") as file:
         json.dump(people, file, indent=2)
+
+
+def random_location_near(person, max_distance_m=50):
+    angle = random.uniform(0, 2 * math.pi)
+
+    # Random distance somewhere inside a 50m circle
+    distance_m = max_distance_m * math.sqrt(random.random())
+
+    latitude_change = (distance_m * math.cos(angle)) / 111_320
+
+    longitude_change = (
+        distance_m * math.sin(angle)
+        / (111_320 * math.cos(math.radians(person["latitude"])))
+    )
+
+    return (
+        person["latitude"] + latitude_change,
+        person["longitude"] + longitude_change
+    )
+
+
+class PersonCreate(BaseModel):
+    name: str
+    age: int
+    role: str = "Hiker"
+    heading: float = 0
+    status: str = "SAFE"
+
 
 @app.get("/")
 def home():
@@ -37,20 +68,27 @@ def get_person(person_id: int):
     raise HTTPException(status_code=404, detail="Person not found")
 
 
-class PersonCreate(BaseModel):
-    name: str
-    age: int
-
-
 @app.post("/people", status_code=201)
 def create_person(new_person: PersonCreate):
     people = load_people()
+
+    leader = next(
+        (person for person in people if person["role"] == "Leader"),
+        None
+    )
+
+    if leader is None:
+        raise HTTPException(status_code=500, detail="No leader exists")
+
+    latitude, longitude = random_location_near(leader)
 
     new_id = max((person["id"] for person in people), default=0) + 1
 
     person_to_add = {
         "id": new_id,
-        **new_person.model_dump()
+        **new_person.model_dump(),
+        "latitude": latitude,
+        "longitude": longitude
     }
 
     people.append(person_to_add)
